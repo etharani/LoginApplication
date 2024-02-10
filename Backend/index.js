ccee8d71f7d55c60954e1a8cb2a1041580f74554
etharani <tharani2297@gmail.com>
2024-02-10 23:13:46 +0530

    Creating user login and registration process

--- Backend/index.js
+++ Backend/index.js
@@ -2,16 +2,23 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-const port = 8080;
+const port = 8088;
+const user = require("./routes/User");
 
 app.use(express.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect('')
+mongoose.connect('mongodb+srv://tharani2297:tharu%404411@tharu.jikk6hd.mongodb.net/Login')
   .then(() => {
     console.log('Successful MongoDB connected');
   })
   .catch((err) => {
     console.error('MongoDB connection error', err);
   });
+
+  app.use(user);
+  
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ Backend/routes/User.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require("../models/user");
+
+// Route for handling user registration
+router.post('/register', async (req, res) => {
+  try {
+    // Fetching values from the request body
+    const {
+      username,
+      password
+    } = req.body;
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10); // Hashing with a salt round of 10
+
+    // Create a new user instance with the hashed password
+    const newUser = new User({
+        username,
+        password: hashedPassword, // Store the hashed password
+    });
+
+    // Save the user to the database
+    await newUser.save();
+
+     // Respond with a success message
+     return res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+      // Handle any errors
+      return res.status(500).json({ error: 'Registration failed' });
+    }
+  });
+
+  //login
+  router.post('/login', async (req, res) => {
+    try {
+      const { username, password } = req.body;
+  
+      // Check if the user exists in the database
+      const user = await User.findOne({ username });
+  
+      if (!user) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+      }
+  
+      // Compare the password using bcrypt or any secure comparison method
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+  
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+      }
+  
+      // If the user and password are valid, simply return a success message
+      return res.status(200).json({ message: 'Login successful',user });
+    } catch (error) {
+      console.error('Login error', error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
+  module.exports = router;

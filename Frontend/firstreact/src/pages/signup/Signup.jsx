866a436b93179e98425a9235de889b33cf3ad1c9
etharani <tharani2297@gmail.com>
2024-02-11 17:58:39 +0530

    Some changes for signup page

--- Frontend/firstreact/src/pages/signup/Signup.jsx
+++ Frontend/firstreact/src/pages/signup/Signup.jsx
@@ -1,34 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Signup.css";
 
 
 function Signup() {
+
+   useState({
+      username:{required:false},
+      password:{required:false}
+   })
   return (
-    <section class="register-block">
-    <div class="container">
-       <div class="row ">
-          <div class="col register-sec">
-             <h2 class="text-center">Register Now</h2>
-             <form class="register-form" action="" >
-             <div class="form-group">
-                <label for="exampleInputEmail1" class="text-uppercase">User Name</label>
-                <input type="text" class="form-control" name="name" id=""  />
-             </div>
-             <div class="form-group">
-                <label for="exampleInputPassword1" class="text-uppercase">Password</label>
-                <input  class="form-control" type="password"  name="password" id="" />
-             </div>
-             <div class="form-group">
-                <input type="submit" class="btn btn-login float-right"  value="Register"/>
-             </div>
-             <div class="form-group">
-                <div class="signup_link">Already a member ? <a href="index.html">Login now</a></div>
-             </div>
-             </form>
-          </div>
-       </div>
+   <div class="container">
+    <div class="register-sec">
+        <h1>Registration</h1>
     </div>
-  </section>
+    <form action="=#">
+    <div class="mb-3">
+          <label for="user" class="form-label">User Name :</label>
+          <input type="text" class="form-control" id="user" placeholder="Enter your User name"/>
+    </div>
+    <div class="mb-3">
+          <label for="password" class="form-label">Password :</label>
+          <input type="password" class="form-control" id="password" placeholder="Enter your Password"/>
+    </div>
+
+         
+            <div class="btn"> 
+            <input type="submit" class="btn btn-login float-right"  value="Register"/>
+            </div>
+            <div class="signup_link">Already a member ? <a href="index.html">Login now</a></div>
+    </form>
+    
+</div>
   )
 }
 

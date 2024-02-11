bfbe01f5eb33292e104b7e6dc14b2bb88f192062
etharani <tharani2297@gmail.com>
2024-02-11 10:51:37 +0530

    Create to frontend part

---
+++ Frontend/firstreact/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Login from './pages/login/Login';
+
+
+function App() {
+  return (
+    <div className="App">
+     hi tharani
+    <Login/>
+
+
+    </div>
+  );
+}
+
+export default App;
